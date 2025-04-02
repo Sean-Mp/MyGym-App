0ce4e37ee1aacce5ce7dc92ec43eac9c16cfc18e
Sean <seanmaritz1304@gmail.com>
2025-04-02 18:53:55 +0200

    Started with home

--- MyGymApp/src/app/home/home.page.ts
+++ MyGymApp/src/app/home/home.page.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { FormsModule} from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { IonContent, IonTabs, IonTabBar, IonTabButton, IonIcon, IonTab, IonHeader, IonToolbar, IonTitle, IonCard } from '@ionic/angular/standalone';
+import { GlobalTabComponent } from '../global-tab/global-tab.component';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCard, IonTitle, IonToolbar, IonHeader, IonTab, IonIcon, IonTabButton, IonTabBar, IonTabs, IonContent, CommonModule, FormsModule, GlobalTabComponent, RouterOutlet]
 })
+
 export class HomePage implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
